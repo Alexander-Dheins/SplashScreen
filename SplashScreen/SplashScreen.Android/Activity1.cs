5ff2f75428abc2fc04d0808c6178b229cd763e4c
Alexander-Dheins <alexanderdheins@hotmail.com>
2022-04-15 20:02:55 -0500

    Agregar archivos de proyecto.

---
+++ SplashScreen/SplashScreen.Android/Activity1.cs
@@ -0,0 +1,26 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SplashScreen.Droid
+{
+    [Activity(Label = "Activity1", Theme = "@style/SplashTheme",
+        MainLauncher = true, NoHistory = true, ConfigurationChanges = ConfigChanges.ScreenSize)]
+    public class Activity1 : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+            // Create your application here
+        }
+    }
+}
